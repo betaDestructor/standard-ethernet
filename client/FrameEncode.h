ca371c40ed078689002e63252c42207978bf8586
P Punyacharan <punyacharan@gmail.com>
2021-08-19 00:04:53 +0530

    Update FrameEncode.h

--- client/FrameEncode.h
+++ client/FrameEncode.h
@@ -1,3 +1,12 @@
+/*
+*---------------------------------------------------------------------------------------------------------------
+*	Author	: P Punyacharan, 18-August-2021
+*	Email	: punyacharan@gmail.com
+*	Licence	: GPL-2.0 License
+*	Purpose	: Header file containing declarations of the classes and methods to assist to encode the ethernet frame.
+*---------------------------------------------------------------------------------------------------------------
+*/
+
 #ifndef _FRAME_ENCODE_H_
 #define _FRAME_ENCODE_H_
 
@@ -24,22 +33,49 @@
 using std::string;
 
 class Frame {
+
+    /*
+        Ethernet frame header:
+        +----------+-----+-----------+----------+------+------------------+-----+
+        | Preamble | SFD | Dest Addr | Src Addr | Type | Data and Padding | CRC |
+        +----------+-----+-----------+----------+------+------------------+-----+
+    */
+
     private:
+        // Preamble should contain 56 bits of alternate 0s and 1s.
         unsigned long int preamble = 0b01010101010101010101010101010101010101010101010101010101;
+
+        // SFD should contain 10101011.
         u_int8_t start_frame_delimiter = 0b010101011;
+        
+        // struct in6_addr sed to store the IPv6 address. Read ipv6(7) manual article for more details.
         struct in6_addr destination_address;
         struct in6_addr source_address;
+
         unsigned int type = 0x86DD; // IPv6
         string payload;
         u_int8_t crc;
     
     public:
+        // Checks if the fetching operation of hostname was successful or not.
         void checkHostName (int hostname);
         void checkHostEntry (struct hostent * hostentry);
+
+        // Returns the string containing the source address in IPv4.
         string getsourceaddress();
+
+        // Returns the index of the next ':' present in the first parameter starting from second parameter as index.
         size_t findNextDelimiter (string &addr, size_t i);
+
+        // Populates the structure member array of source_address which is the data member of this class.
+        // Reads string containing hex values separated with ':' delimiter
         void populate_source_addr (string addr);
+
+        // Populates the structure member array of dest_address which is the data member of this class.
+        // Reads string containing hex values separated with ':' delimiter
         void populate_dest_addr (string addr);
+
+        // Calculates the CRC of the payload and populates it to the corresponding member data of this class.
         void populateCRC();
 
 		// Getters
