358ec7313b7ba8088f8fae6b070638dc89b2e831
P Punyacharan <punyacharan@gmail.com>
2021-08-19 00:05:40 +0530

    Update main.cpp

--- client/main.cpp
+++ client/main.cpp
@@ -1,3 +1,12 @@
+/*
+*---------------------------------------------------------------------------------------------------------------
+*	Author	: P Punyacharan, 18-August-2021
+*	Email	: punyacharan@gmail.com
+*	Licence	: GPL-2.0 License
+*	Purpose	: Driver file for the client end of Ethernet protocol.
+*---------------------------------------------------------------------------------------------------------------
+*/
+
 #include <bits/stdc++.h>
 #include <fcntl.h>
 #include <sys/stat.h>
@@ -5,9 +14,16 @@
 #include "FrameEncode.h"
 
 using namespace std;
+
+// Hardcoded source address.
 const string SOURCE_ADDR = "4A:30:10:21:10:1A";
 
+// Prints the input binary string in chunks of 4 bits so that it is readable.
 void printBinary (string binary) {
+
+	// Inorder to print input binary in chunks of 4 bits, the length of the input binary
+    // should be an integral multiple of 4. The conditional pads couple of 0s to make the length
+    // an integral multiple of 4 if not so already.
 	int padding = binary.length() % 4;
 	if (padding != 0) {
 		string padbits = "";
@@ -16,6 +32,8 @@ void printBinary (string binary) {
 		binary = padbits + binary;
 	}
 	cout << endl;
+
+	// Prints the binary in chunks of 4.
 	for (size_t i = 0 ; i < binary.length() ; i+=4) {
 		for (size_t j = i ; j < i + 4 ; j++)
 			cout << binary[j];
@@ -38,7 +56,7 @@ int main() {
 			cin >> dest_address;
 			break;
 		case 2: cout << "You have chosen broadcast." << endl;
-			dest_address = "FF:FF:FF:FF:FF:FF";
+			dest_address = "FF:FF:FF:FF:FF:FF"; // Default address for broadcast.
 			break;
 		default: cout << "INVALID CHOICE. Exiting" << endl; exit(1);
 	}
@@ -97,32 +115,16 @@ int main() {
     // mkfifo(<pathname>, <permission>)
     mkfifo(myfifo, 0666);
 
-    // while (1)
-    {
-        // Open FIFO for write only
-        fd = open(myfifo, O_WRONLY);
+    // Open FIFO for write only
+    fd = open(myfifo, O_WRONLY);
 
-        // Take an input arr2ing from user.
-        // 80 is maximum length
-        // fgets(arr2, 80, stdin);
-		char *binaryFrameCharArray = (char *) calloc (binaryFrame.length() + 1, sizeof(char));
-		strcpy(binaryFrameCharArray, binaryFrame.c_str());
-        // Write the input arr2ing on FIFO
-        // and close it
-        write(fd, binaryFrameCharArray, binaryFrame.length() + 1);
-		free(binaryFrameCharArray);
-        close(fd);
-
-        // Open FIFO for Read only
-        // fd = open(myfifo, O_RDONLY);
-
-        // Read from FIFO
-        // read(fd, binaryFrame, sizeof(arr1)); // Read into a char array
-
-        // Print the read message
-        // printf("User2: %s\n", binaryFrame);
-        // close(fd);
-    }
+	char *binaryFrameCharArray = (char *) calloc (binaryFrame.length() + 1, sizeof(char));
+	strcpy(binaryFrameCharArray, binaryFrame.c_str());
+    
+	// Write the input binaryFrameCharArray on FIFO and close it
+    write(fd, binaryFrameCharArray, binaryFrame.length() + 1);
+	free(binaryFrameCharArray);
+    close(fd);
 
     delete data;
 	return 0;
