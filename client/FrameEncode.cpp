18d035d5ed4249f2cd6495fb4e981ae0faf3e130
P Punyacharan <punyacharan@gmail.com>
2021-08-19 00:04:29 +0530

    Update FrameEncode.cpp

--- client/FrameEncode.cpp
+++ client/FrameEncode.cpp
@@ -1,3 +1,12 @@
+/*
+*---------------------------------------------------------------------------------------------------------------
+*	Author	: P Punyacharan, 18-August-2021
+*	Email	: punyacharan@gmail.com
+*	Licence	: GPL-2.0 License
+*	Purpose	: Implementation file of FrameEncode.h header file.
+*---------------------------------------------------------------------------------------------------------------
+*/
+
 #include <bits/stdc++.h>
 #include "FrameEncode.h"
 
@@ -6,6 +15,85 @@ using namespace std;
 const unsigned char CRC7_POLY = 0x91;
 const int MAX_PAYLOAD_LENGTH = 1500;
 
+// Checks if the fetching operation of hostname was successful or not.
+void Frame::checkHostName (int hostname) {
+	if (hostname == -1) {
+		cout << "gethostname error";
+		exit(EXIT_FAILURE);
+	}
+}
+
+void Frame::checkHostEntry (struct hostent * hostentry) {
+	if (hostentry == NULL) {
+		cout << "gethostname error";
+		exit(EXIT_FAILURE);
+	}
+}
+
+// Returns the string containing the source address in IPv4.
+string Frame::getsourceaddress() {
+	struct hostent *host_entry;
+	char hostbuffer[256];
+	int hostname = gethostname(hostbuffer, sizeof(hostbuffer));
+	checkHostName(hostname);
+	host_entry = gethostbyname(hostbuffer);
+	checkHostEntry(host_entry);
+	char *IPbuffer = inet_ntoa(*((struct in_addr*) host_entry->h_addr_list[0]));
+	string addr;
+	for (size_t i = 0 ; i < strlen(IPbuffer) ; i++)
+		addr += IPbuffer[i];
+	return addr;
+	// cout << "Host name: " << hostbuffer << endl;
+	// cout << "IP: " << IPbuffer;
+}
+
+// Returns the index of the next ':' present in the first parameter starting from second parameter as index.
+size_t Frame::findNextDelimiter (string &addr, size_t i) {
+    while (i < addr.length() && addr.at(i++) != ':');
+    return i - 1;
+}
+
+// Populates the structure member array of source_address which is the data member of this class.
+// Reads string containing hex values separated with ':' delimiter
+void Frame::populate_source_addr (string addr) {
+	for (size_t i = 0, j = 0 ; i < addr.length() ;)
+    {
+        size_t k = findNextDelimiter(addr, i);
+        (this->source_address).__in6_u.__u6_addr16[j++] = stoi(addr.substr(i, i-k), 0, 16);
+        i = k + 1;
+    }
+}
+
+// Populates the structure member array of dest_address which is the data member of this class.
+// Reads string containing hex values separated with ':' delimiter
+void Frame::populate_dest_addr (string addr) {
+	for (size_t i = 0, j = 0 ; i < addr.length() ;)
+    {
+        size_t k = findNextDelimiter(addr, i);
+        (this->destination_address).__in6_u.__u6_addr16[j++] = stoi(addr.substr(i, i-k), 0, 16);
+        i = k + 1;
+    }
+}
+
+// Calculates the CRC of the payload and populates it to the corresponding member data of this class.
+void Frame::populateCRC()
+{
+	unsigned char i, j, crc = 0;
+	for (i = 0; i < (this->payload).length(); i++)
+	{
+		crc ^= (this->payload)[i];
+		for (j = 0; j < 8; j++)
+		{
+			if (crc & 1)
+				crc ^= CRC7_POLY;
+			crc >>= 1;
+		}
+	}
+	(this->crc) = crc;
+}
+
+
+// ---------------GETTERS-----------------
 string Frame::getBinaryPreamble() {
 	return bitset<PREAMBLE_LEN>(this->preamble).to_string();
 }
@@ -62,75 +150,6 @@ string Frame::getBinaryPayload() {
 
 string Frame::getBinaryCRC() {
 	return bitset<CRC_LEN>(this->crc).to_string();
-}
-
-void Frame::populateCRC()
-{
-	unsigned char i, j, crc = 0;
-	for (i = 0; i < (this->payload).length(); i++)
-	{
-		crc ^= (this->payload)[i];
-		for (j = 0; j < 8; j++)
-		{
-			if (crc & 1)
-				crc ^= CRC7_POLY;
-			crc >>= 1;
-		}
-	}
-	(this->crc) = crc;
-}
-
-void Frame::checkHostName (int hostname) {
-	if (hostname == -1) {
-		cout << "gethostname error";
-		exit(1);
-	}
-}
-
-void Frame::checkHostEntry (struct hostent * hostentry) {
-	if (hostentry == NULL) {
-		cout << "gethostname error";
-		exit(1);
-	}
-}
-
-string Frame::getsourceaddress() {
-	struct hostent *host_entry;
-	char hostbuffer[256];
-	int hostname = gethostname(hostbuffer, sizeof(hostbuffer));
-	checkHostName(hostname);
-	host_entry = gethostbyname(hostbuffer);
-	checkHostEntry(host_entry);
-	char *IPbuffer = inet_ntoa(*((struct in_addr*) host_entry->h_addr_list[0]));
-	string addr;
-	for (size_t i = 0 ; i < strlen(IPbuffer) ; i++)
-		addr += IPbuffer[i];
-	return addr;
-	// cout << "Host name: " << hostbuffer << endl;
-	// cout << "IP: " << IPbuffer;
-}
-
-size_t Frame::findNextDelimiter (string &addr, size_t i) {
-    while (i < addr.length() && addr.at(i++) != ':');
-    return i - 1;
-}
-
-void Frame::populate_source_addr (string addr) {
-	for (size_t i = 0, j = 0 ; i < addr.length() ;)
-    {
-        size_t k = findNextDelimiter(addr, i);
-        (this->source_address).__in6_u.__u6_addr16[j++] = stoi(addr.substr(i, i-k), 0, 16);
-        i = k + 1;
-    }
-}
-
-void Frame::populate_dest_addr (string addr) {
-	for (size_t i = 0, j = 0 ; i < addr.length() ;)
-    {
-        size_t k = findNextDelimiter(addr, i);
-        (this->destination_address).__in6_u.__u6_addr16[j++] = stoi(addr.substr(i, i-k), 0, 16);
-        i = k + 1;
-    }
 }
 
 Frame::~Frame() {}
